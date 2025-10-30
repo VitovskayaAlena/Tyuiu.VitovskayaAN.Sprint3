f69cefa121ca7f2f39451c81dedf8f58c4fc9268
qqbelll <alenkav07@listru>
2025-10-30 23:23:19 +0500

    Task4

---
+++ Tyuiu.VitovskayaAN.Sprint3.Task4.V10.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.VitovskayaAN.Sprint3.Task4.V10.Lib;
+namespace Tyuiu.VitovskayaAN.Sprint3.Task4.V10.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int x = -5;
+            int y = 5;
+            Assert.AreEqual(0.014, ds.Calculate(x, y));
+        }
+    }
+}
---
+++ Tyuiu.VitovskayaAN.Sprint3.Task4.V10/Program.cs
@@ -0,0 +1,35 @@
+﻿using Tyuiu.VitovskayaAN.Sprint3.Task4.V10.Lib;
+class Program
+{
+    static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        Console.Title = "Спринт #3 | Выполнила: Витовская А. Н. | РППб-25-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #3                                                               *");
+        Console.WriteLine("* Тема: Использование операторов continue и break в циклах                *");
+        Console.WriteLine("* Задание #4                                                              *");
+        Console.WriteLine("* Вариант #10                                                             *");
+        Console.WriteLine("* Выполнила: Витовская Алёна Николаевна | РППб-25-1                       *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* На отрезке, где x принимает значения от -5 до 5, вычислить              *");
+        Console.WriteLine("* значение функции                                                        *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        Console.WriteLine("Введите стартовое значение: ");
+        int y = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введите конечное значение: ");
+        int z = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        Console.WriteLine(ds.Calculate(y, z));
+
+        Console.ReadKey();
+    }
+}
