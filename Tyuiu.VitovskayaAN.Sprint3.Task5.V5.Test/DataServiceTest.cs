cbc12923f52ab2d3c21588d11478bed969b85a9d
qqbelll <alenkav07@listru>
2025-10-31 00:34:55 +0500

    Task5

---
+++ Tyuiu.VitovskayaAN.Sprint3.Task5.V5.Test/DataServiceTest.cs
@@ -0,0 +1,15 @@
+﻿using Tyuiu.VitovskayaAN.Sprint3.Task5.V5.Lib;
+namespace Tyuiu.VitovskayaAN.Sprint3.Task5.V5.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            double res = ds.GetSumSumSeries(5, 1, 1, 3, 10);
+            Assert.AreEqual(98.286, res);
+        }
+    }
+}
---
+++ Tyuiu.VitovskayaAN.Sprint3.Task5.V5/Program.cs
@@ -0,0 +1,36 @@
+﻿using Tyuiu.VitovskayaAN.Sprint3.Task5.V5.Lib;
+class Program
+{
+    static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        Console.Title = "Спринт #3 | Выполнила: Витовская А. Н. | РППб-25-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #3                                                               *");
+        Console.WriteLine("* Тема: Использование операторов continue и break в циклах                *");
+        Console.WriteLine("* Задание #5                                                              *");
+        Console.WriteLine("* Вариант #5                                                              *");
+        Console.WriteLine("* Выполнила: Витовская Алёна Николаевна | РППб-25-1                       *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        Console.WriteLine("Значение переменной X = 5");
+        Console.WriteLine("Начальный шаг1 = переменая i = 1");
+        Console.WriteLine("Начальный шаг2 = переменная k = 1");
+        Console.WriteLine("Конечный шаг1 = 3");
+        Console.WriteLine("Конечный шаг2 = 10");
+
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        double res = ds.GetSumSumSeries(5, 1, 1, 3, 10);
+        Console.WriteLine("Сумма ряда = " + res);
+    }
+}
