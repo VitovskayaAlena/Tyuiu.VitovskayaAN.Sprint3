e2ede6b435908c8801ac5c42570e50d9acc1da08
qqbelll <alenkav07@listru>
2025-10-30 22:03:57 +0500

    Task3

---
+++ Tyuiu.VitovskayaAN.Sprint3.Task3.V11.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Tyuiu.VitovskayaAN.Sprint3.Task3.V11.Lib;
+namespace Tyuiu.VitovskayaAN.Sprint3.Task3.V11.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidReplaceCharOnNum()
+        {
+            DataService ds = new DataService();
+            string str = "sqwreq vqwqq q";
+            char x = 'q';
+            char y = '7';
+
+            string res = ds.ReplaceCharOnNum(str, x, y);
+            string wait = "s7wre7 v7w77 7";
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.VitovskayaAN.Sprint3.Task3.V11/Program.cs
@@ -0,0 +1,37 @@
+﻿using Tyuiu.VitovskayaAN.Sprint3.Task3.V11.Lib;
+class Program
+{
+    static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        Console.Title = "Спринт #3 | Выполнила: Витовская А. Н. | РППб-25-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #3                                                               *");
+        Console.WriteLine("* Тема: Оператор цикла foreach                                            *");
+        Console.WriteLine("* Задание #3                                                              *");
+        Console.WriteLine("* Вариант #11                                                             *");
+        Console.WriteLine("* Выполнила: Витовская Алёна Николаевна | РППб-25-1                       *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Используя цикл foreach заменить буквы q на цифру 7 в строке             *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        string str = "sqwreq vqwqq q";
+        char x = 'q';
+        char y = '7';
+
+        Console.WriteLine("Изначальная строка: " + str);
+        Console.WriteLine("Заменяемый символ: " + x);
+        Console.WriteLine("Символ, которым заменяют: " + y);
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ                                                               *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("Новая строка " + ds.ReplaceCharOnNum(str, x, y));
+
+        Console.ReadLine();
+    }
+}
