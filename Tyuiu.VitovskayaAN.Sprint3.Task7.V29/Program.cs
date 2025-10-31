06e2ab881d9f7beab78516dae007e5805d790b6a
qqbelll <alenkav07@listru>
2025-11-01 01:13:48 +0500

    Task7

--- Tyuiu.VitovskayaAN.Sprint3.Task7.V29/Program.cs
+++ Tyuiu.VitovskayaAN.Sprint3.Task7.V29/Program.cs
@@ -33,15 +33,15 @@ class Program
         Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
         Console.WriteLine("***************************************************************************");
 
-        Console.WriteLine("+----------+-----------+");
-        Console.WriteLine("|    X     |   f(x)    |");
-        Console.WriteLine("+----------+-----------+");
+        Console.WriteLine("+----------+------------+");
+        Console.WriteLine("|    X     |   f(x)     |");
+        Console.WriteLine("+----------+------------+");
         for (int i = 0; i <= len - 1; i++)
         {
-            Console.WriteLine("|{0,5:d}     |  {1, 5:f2}    |", startValue, valueArray[i]);
+            Console.WriteLine("|{0,5:d}     |  {1, 6:f2}    |", startValue, valueArray[i]);
             startValue++;
         }
-        Console.WriteLine("+----------+-----------+");
+        Console.WriteLine("+----------+------------+");
         Console.ReadKey();
     }
 }
